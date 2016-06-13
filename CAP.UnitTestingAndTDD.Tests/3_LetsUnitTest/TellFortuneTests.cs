1cdb69fb137d4ee0f4f2ea83d12a2c17be42b419
Steven Hicks <steven.hicks@centare.com>
2016-06-12 23:21:52 -0500

    let's unit test!

---
+++ CAP.UnitTestingAndTDD.Tests/3_LetsUnitTest/TellFortuneTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CAP.UnitTestingAndTDD._3_LetsUnitTest;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CAP.UnitTestingAndTDD.Tests._3_LetsUnitTest
+{
+    [TestClass]
+    public class TellFortuneTests
+    {
+        [TestMethod]
+        public void GivenNullName_ReturnsNoFuture()
+        {
+            var fortuneTeller = new FortuneTeller();
+
+            var result = fortuneTeller.TellFortune(null);
+
+            Assert.AreEqual(result, FortuneTeller.NoFuture);
+        }
+    }
+}
---
+++ CAP.UnitTestingAndTDD/3_LetsUnitTest/FortuneTeller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAP.UnitTestingAndTDD._3_LetsUnitTest
+{
+    public class FortuneTeller
+    {
+        public string TellFortune(string yourName)
+        {
+            if (string.IsNullOrWhiteSpace(yourName))
+            {
+                return NoFuture;
+            }
+
+            if (yourName.Length == 5)
+            {
+                if (yourName.StartsWith("S"))
+                {
+                    return Awesome;
+                }
+                return LiveForever;
+            } else if (yourName.Length == 9)
+            {
+                return LongName;
+            }
+
+            return Mediocre;
+        }
+
+        public const string NoFuture = "You have no future";
+        public const string Mediocre = "You are destined for mediocrity.";
+        public const string Awesome = "You are awesome and everyone likes you!";
+        public const string LiveForever = "May you live to be a thousand years old.";
+        public const string LongName = "May your life be as long as your name.";
+    }
+}
