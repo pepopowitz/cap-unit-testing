c7145499c1da68d74608f86edfa635545d75a670
Steven Hicks <steven.hicks@centare.com>
2016-06-13 22:32:15 -0500

    set up fizzbuzz

---
+++ CAP.UnitTestingAndTDD.Tests/7_FizzBuzz/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CAP.UnitTestingAndTDD._7_FizzBuzz;
+using NUnit.Framework;
+
+namespace CAP.UnitTestingAndTDD.Tests._7_FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+
+    }
+}
---
+++ CAP.UnitTestingAndTDD/7_FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAP.UnitTestingAndTDD._7_FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string FizzBuzz(int value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
